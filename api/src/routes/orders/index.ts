92d7d5fc6160d9e08d09d592c0f8faf56b6eebf9
mohvmed99 <mohamedsufyanx@gmail.com>
2025-09-16 01:22:29 +0400

    Fix order validation and nested items

--- api/src/routes/orders/index.ts
+++ api/src/routes/orders/index.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { createOrder, getOrder, listOrders } from './ordersController.js';
 import { validateData } from '../../middlewares/validationMiddleware.js';
-import { insertOrderItemSchema, updateOrderSchema } from '../../db/ordersSchema.js';
+import { insertOrderWithItemsSchema, updateOrderSchema } from '../../db/ordersSchema.js';
 import { verifyToken } from '../../middlewares/authMiddleware.js';
 
 
 const router = Router();
 
-router.post('/', verifyToken, validateData(insertOrderItemSchema), createOrder);
+router.post('/', verifyToken, validateData(insertOrderWithItemsSchema), createOrder);
 
 router.get('/', verifyToken, listOrders);
 router.get('/:id', verifyToken, getOrder);
